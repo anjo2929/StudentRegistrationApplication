a3c1b33b4b066f96c852b4b1dd29090384f9b6de
Joan <Joan@DESKTOP-D4KOPVH>
2024-10-07 22:00:45 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace StudentRegistrationApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lastname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.firstname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.middlename = new System.Windows.Forms.TextBox();
+            this.male = new System.Windows.Forms.RadioButton();
+            this.female = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.programtoapply = new System.Windows.Forms.ComboBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.browse = new System.Windows.Forms.Button();
+            this.register = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Registration Form";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name*";
+            // 
+            // lastname
+            // 
+            this.lastname.Location = new System.Drawing.Point(16, 71);
+            this.lastname.Name = "lastname";
+            this.lastname.Size = new System.Drawing.Size(123, 20);
+            this.lastname.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "First Name*";
+            // 
+            // firstname
+            // 
+            this.firstname.Location = new System.Drawing.Point(16, 131);
+            this.firstname.Name = "firstname";
+            this.firstname.Size = new System.Drawing.Size(123, 20);
+            this.firstname.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Middle Name*";
+            // 
+            // middlename
+            // 
+            this.middlename.Location = new System.Drawing.Point(16, 192);
+            this.middlename.Name = "middlename";
+            this.middlename.Size = new System.Drawing.Size(123, 20);
+            this.middlename.TabIndex = 6;
+            // 
+            // male
+            // 
+            this.male.AutoSize = true;
+            this.male.Location = new System.Drawing.Point(91, 229);
+            this.male.Name = "male";
+            this.male.Size = new System.Drawing.Size(48, 17);
+            this.male.TabIndex = 7;
+            this.male.TabStop = true;
+            this.male.Text = "Male";
+            this.male.UseVisualStyleBackColor = true;
+            // 
+            // female
+            // 
+            this.female.AutoSize = true;
+            this.female.Location = new System.Drawing.Point(145, 229);
+            this.female.Name = "female";
+            this.female.Size = new System.Drawing.Size(59, 17);
+            this.female.TabIndex = 8;
+            this.female.TabStop = true;
+            this.female.Text = "Female";
+            this.female.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Gender:";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(13, 266);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 23);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Date Of Birth*";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(16, 292);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(12, 327);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(159, 23);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Program to Apply";
+            // 
+            // programtoapply
+            // 
+            this.programtoapply.FormattingEnabled = true;
+            this.programtoapply.Items.AddRange(new object[] {
+            "BS in Information Technology",
+            "BS in Computer Science",
+            "BS in Business Administration",
+            "BS in Accountancy",
+            "BS in Hospitality Management"});
+            this.programtoapply.Location = new System.Drawing.Point(16, 353);
+            this.programtoapply.Name = "programtoapply";
+            this.programtoapply.Size = new System.Drawing.Size(252, 21);
+            this.programtoapply.TabIndex = 13;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox.Location = new System.Drawing.Point(513, 26);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(275, 237);
+            this.pictureBox.TabIndex = 14;
+            this.pictureBox.TabStop = false;
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(610, 289);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(75, 23);
+            this.browse.TabIndex = 15;
+            this.browse.Text = "Browse";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // register
+            // 
+            this.register.BackColor = System.Drawing.Color.Red;
+            this.register.Location = new System.Drawing.Point(16, 380);
+            this.register.Name = "register";
+            this.register.Size = new System.Drawing.Size(155, 43);
+            this.register.TabIndex = 16;
+            this.register.Text = "Register student";
+            this.register.UseVisualStyleBackColor = false;
+            this.register.Click += new System.EventHandler(this.register_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.register);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.programtoapply);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.female);
+            this.Controls.Add(this.male);
+            this.Controls.Add(this.middlename);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.firstname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lastname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lastname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox firstname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox middlename;
+        private System.Windows.Forms.RadioButton male;
+        private System.Windows.Forms.RadioButton female;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox programtoapply;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.Button register;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StudentRegistrationApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            programtoapply.Items.Add("BS in Information Technology");
+            programtoapply.Items.Add("BS in Computer Science");
+            programtoapply.Items.Add("BS in Business Administration");
+            programtoapply.Items.Add("BS in Accountancy");
+            programtoapply.Items.Add("BS in Hospitality Management");
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Image = new Bitmap(openFileDialog.FileName);
+            }
+        }
+
+        private void register_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(firstname.Text) ||
+                string.IsNullOrEmpty(lastname.Text) ||
+                programtoapply.SelectedItem == null)
+            {
+                MessageBox.Show("Please fill in all required fields.", "Validation Error");
+                return;
+            }
+
+            string firstName = firstname.Text;
+            string middleName = middlename.Text;
+            string lastName = lastname.Text;
+            string gender = male.Checked ? "Male" : "Female";
+            DateTime dob = dateTimePicker.Value;
+            string program = programtoapply.SelectedItem.ToString();
+
+            DisplayStudentInfo(firstName, lastName); 
+            DisplayStudentInfo(firstName, middleName, lastName, gender); 
+            DisplayStudentInfo(firstName, middleName, lastName, gender, dob, program);
+
+           
+            ClearForm();
+        }
+
+        private void ClearForm()
+        {
+            firstname.Text = "";
+            middlename.Text = "";
+            lastname.Text = "";
+            male.Checked = false;
+            female.Checked = false;
+            dateTimePicker.Value = DateTime.Now;
+            programtoapply.SelectedIndex = -1; 
+            pictureBox.Image = null; 
+        }
+
+        public void DisplayStudentInfo(string firstName, string lastName)
+        {
+            MessageBox.Show($"Student name: {firstName} {lastName}");
+        }
+
+        public void DisplayStudentInfo(string firstName, string middleName, string lastName, string gender)
+        {
+            MessageBox.Show($"Student name: {firstName} {middleName} {lastName}\nGender: {gender}");
+        }
+  
+        public void DisplayStudentInfo(string firstName, string middleName, string lastName, string gender, DateTime dob, string program)
+        {
+            MessageBox.Show($"Student name: {firstName} {middleName} {lastName}\nGender: {gender}\nDate of birth: {dob.ToShortDateString()}\nProgram: {program}");
+        }
+    }
+}
